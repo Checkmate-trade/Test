2be2db872985a4427daa55bdbd7f4461e569a3ba
Checkmate-trade <157131810+Checkmate-trade@users.noreply.github.com>
2025-01-12 13:04:41 +0900

    Push Test
    Test

---
+++ MyActor.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class TEST1_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	int32 number1 = 10;
+
+	UPROPERTY(VisibleAnywhere)
+	float number2 = 3.14f;
+
+	//FString name = "Park Won Seok";
+	FString name = TEXT("박원석");
+	bool isReady = true;
+	bool isFinished = false;
+};
